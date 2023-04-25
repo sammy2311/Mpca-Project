075d4a9dd1b7b6497e922ae607da66a5d6e81d9b
sammy2311 <samarthingalagavi@gmail.com>
2023-04-25 18:49:09 +0530

    Entire Project

---
+++ Db.py
@@ -0,0 +1,36 @@
+import Display
+from pymongo.mongo_client import MongoClient
+
+uri = "mongodb+srv://strawberriessss:berriessss@mpca.vzofeaw.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(uri)
+db = client.MPCA
+collection = db.users
+
+def insert(obj):
+    i = collection.insert_one(obj)
+    print("inserted", i)
+    
+def find(obj):
+    record = collection.find_one(obj)
+    return record
+    
+def update(price, obj):
+    record = find(obj)
+    print(record)
+    curr = record['balance']
+    if(curr-price>=0):
+        collection.update_one(record, {"$set":{"balance":curr-price}})
+        Display.showText("PAID SUCCESSFULLY", 2)
+        return "1"
+    else:
+        Display.showText("NOT ENOUGH BALANCE", 2)
+        return "0"
+                
+def delete(i):
+    collection.delete_one({'fingerid': i})
+
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
